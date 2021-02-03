d64ba3cd0e445458412df9588190bcf5e8a2ddb9
Alexey Tarabanov <tarabanov@bk.ru>
2021-02-03 13:00:23 +0300

    7/18

--- CommandExecutor.java
+++ CommandExecutor.java
@@ -6,30 +6,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CommandExecutor {
-
-    private static final Map<Operation, Command> ALL_KNOWN_COMMANDS_MAP;
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
 
     static {
-        ALL_KNOWN_COMMANDS_MAP = new HashMap<>();
-
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.CREATE, new ZipCreateCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.ADD, new ZipAddCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.REMOVE, new ZipRemoveCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.EXTRACT, new ZipExtractCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.CONTENT, new ZipContentCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.EXIT, new ExitCommand());
+        allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
+        allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
     }
 
-    private CommandExecutor(Map<Operation, Command> ALL_KNOWN_COMMANDS_MAP) {
-        // чтобы запретить явный вызов конструктора
+    private CommandExecutor() {
     }
 
-    // найдет нужную команду и вызовет у нее метод execute.
     public static void execute(Operation operation) throws Exception {
-        for (Map.Entry<Operation, Command> pair : ALL_KNOWN_COMMANDS_MAP.entrySet()) {
-            if (operation.equals(pair.getKey())) {
-                pair.getValue().execute();
-            }
-        }
+        allKnownCommandsMap.get(operation).execute();
     }
 }
